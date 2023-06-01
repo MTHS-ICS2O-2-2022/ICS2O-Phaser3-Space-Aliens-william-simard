cd9bcdcf6a397643a44aa50cc8be6d43b249dfe1
william <124389603+william-simard@users.noreply.github.com>
2023-06-01 15:16:01 +0000

    done

--- js/game.js
+++ js/game.js
@@ -5,6 +5,16 @@
 // Created by: william simard
 // Created on: may 2023
 // This file contains the JS functions for index.html
+
+// scene import statments
+import splashScene from "./scenes/splashScene.js"
+
+// create the new scene
+const splash = new splashScene()
+
+/**
+ * start phaser game
+ */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
@@ -25,4 +35,11 @@ const config = {
 }
 
 const game = new Phaser.Game(config)
-console.log(game)
+//console.log(game)
+
+// load scenes
+//note: remember any "key" is global and CAN NOT be reused!
+game.scene.add("splashScene", splashScene)
+
+//start game
+game.scene.start("splashScene")
